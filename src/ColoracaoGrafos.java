023a53a855437dc2f0fbf5c6061ddb7172e78e3f
Arley Prates <arleyprates@gmail.com>
2015-04-03 11:15:00 -0300

    implement cor

--- src/ColoracaoGrafos.java
+++ src/ColoracaoGrafos.java
@@ -10,6 +10,7 @@ public class ColoracaoGrafos {
 	private ArrayList<Aresta> arestas;
 	private ArrayList<Cor> cores;
 	private ArrayList<Vertice> adjacentes;
+	private ArrayList<Vertice> q;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,6 +21,8 @@ public class ColoracaoGrafos {
 	public void start() {
 	
 		vertices = new ArrayList<Vertice>();
+		
+		q = new ArrayList<Vertice>();
 		
 		adjacentes = new ArrayList<Vertice>();
 		
@@ -75,9 +78,7 @@ public class ColoracaoGrafos {
 		String a3 = "CD";
 		String a4 = "DB";
 		String a5 = "DE";
-		String a6 = "DA";
-		
-		
+		String a6 = "DA";		
 		
 		//instancia aresta, add o nome, o vertice origem, o vertice destino e add aresta na lista arestas
 		Aresta e = new Aresta(a1, vertices.get(0), vertices.get(1));		
@@ -97,7 +98,7 @@ public class ColoracaoGrafos {
 		
 		e = new Aresta(a6, vertices.get(3), vertices.get(4));		
 		arestas.add(e);
-		
+
 		//intera na lista de vertices
 		System.out.print("V: { ");
 		for (Vertice vertice : vertices) {
@@ -124,32 +125,82 @@ public class ColoracaoGrafos {
 		}
 		System.out.println("}");
 		
-		//ordenar a LA
-		/*
-		System.out.println("LA: {");
-		for (Vertice vertice : vertices) {
-			System.out.print(vertice.getNome() +" -> ");
-			for (Vertice adjacente : vertice.getAdjacencia()) {
-					System.out.print(adjacente.getNome() + " ");
-			} 
-			System.out.print(vertice.getgrau() + " ");
-			System.out.println("");
-		}
-		System.out.println("}");*/
-		
+		//ordenar a vertices pelo grau		
 		Collections.sort(vertices, new CompareVerticeAdjacentes());
 		
-		//intera na lista de vertices
+		//intera na lista de vertices ordanada
 		System.out.print("VO: { ");
 		for (Vertice vertice : vertices) {
 			System.out.print(vertice.getNome() + " ");
 		}
 		System.out.println("}");
+		
+		//instancia lista cores
+		populacores();
+		
+		/*for (Cor c : cores) {
+			System.out.println(c.getCor());
+		}*/
 				
 		//pegar o vertice de maior grau e colori
+		for (Vertice vi : vertices) {
+			Colore_Vertice(vi);				
+		}
+		
+		//intera na lista de vertices
+		System.out.print("V: { ");
+		for (Vertice vertice : vertices) {
+			System.out.print(vertice.getNome() + " " + vertice.getCor().getCor() + "-");
+			for (Vertice ad1 : vertice.getAdjacencia()) {
+				System.out.println(ad1.getCor().getCor() + "- ");
+			}
+		}
+		System.out.println("}");
 	}
 	
-	class CompareVerticeAdjacentes implements Comparator {
+	private void populacores() {
+		// TODO Auto-generated method stub
+		cores.clear();
+		cores.add(new Cor("#ff0000", "#ffffff"));
+		cores.add(new Cor("#ff00ff", "#000000"));
+		cores.add(new Cor("#0000ff", "#ffffff"));
+		cores.add(new Cor("#00ffff", "#000000"));
+		cores.add(new Cor("#ffff00", "#000000"));
+		cores.add(new Cor("#000000", "#ffffff"));
+	}
+
+	private void Colore_Vertice(Vertice vk) {
+		//vertice de maior grau n eh colorido?
+		if (!vk.colorido()) {
+			//intera lista de cores
+			for (Cor cor : cores) {
+				boolean podeusar = true;
+				//intera nos adjacentes de vk
+				for (Vertice vj : vk.getAdjacencia()) {
+					//adjacente de vk tem cor?
+					if (vj.colorido()) {
+						//cor de vj eh igual a cor atual da lista
+						if (vj.getCor().equals(cor)){
+							podeusar = false;
+							//adjacente de cor igual. intera!
+							break;
+						}
+					}
+				}
+				//adjacente sem cor, atribua cor
+				if (podeusar) {
+					vk.setCor(cor);
+					break;
+				}
+			}
+			//ultimo adjacente sem cor
+			for (Vertice vj : vk.getAdjacencia()) {
+				Colore_Vertice(vj);
+			}
+		}
+	}
+
+	private class CompareVerticeAdjacentes implements Comparator {
 		
 		public int compare(Object o1, Object o2) {
 			// TODO Auto-generated method stub
